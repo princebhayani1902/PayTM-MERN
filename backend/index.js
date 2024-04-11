a3b077b6b0e5ab73e640d44a323c8865b76fbb25
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-12 02:58:56 +0530

    Add cors, body parser and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
 import express from "express";
-import { connectToMongoDB } from "./db/connectToMongoDB.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
+import { connectToMongoDB } from "./db/connectToMongoDB.js";
 import rootRouter from "./routes/index.routes.js"
 
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-
 
 app.use("/api/v1", rootRouter);
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     connectToMongoDB();
-    console.log("Server is running");
+    console.log(`Server is running on ${PORT}`);
 })
