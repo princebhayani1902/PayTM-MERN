0344769293a1c22860f092be7bc63888131280fe
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-12 06:03:03 +0530

    router for accounts

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./user.routes.js";
+import accountRouter from "./accounts.routes.js";
 
 const router = express.Router();
 
 router.use("/user",userRouter);
+router.use("/account",accountRouter);
 
 export default router;
