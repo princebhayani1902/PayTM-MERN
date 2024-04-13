7b3f66f00b18484b5c43754d9a2910c7f5a092f2
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-14 02:06:41 +0530

    done with basic things (signup, search user, send money)

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,16 @@
+import { Appbar } from "../components/Appbar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/User"
 
-function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+
+const Dashboard = () => {
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
+    </div>
 }
 
 export default Dashboard
